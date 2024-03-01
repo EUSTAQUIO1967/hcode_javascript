5917d297b33f6d259878aa8c77d309ff7a2a102d
eustaquio santos <eujosan@hotmail.com>
2024-02-29 22:34:03 -0300

    primeiro commit dos codigos do treinamento udemy-hcode calculadora

---
+++ scripts/calculator.js
@@ -0,0 +1,14 @@
+window.calculator = new CalcController();
+
+/** Orientação a objetos */
+// Classes 
+// objeto = new nomeDaClasse();
+// Reutilização de código
+// mais pratico -
+// melhor forma de otimizar código é utilizando orientação objetos
+//
+//  MVC - MODEL VIEW CONTROLLER
+// 
+
+
+
